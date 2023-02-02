3f8fcd05271599e390d0b9e9a6b01d229d83e627
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-02-02 17:00:48 +0100

    Creato un bottone per tornare alla pagina iniziale;

--- resources/views/pages/saint.blade.php
+++ resources/views/pages/saint.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.main-layout')
 
 @section('content')
-<h1 class="text-danger">Saint Selected</h1>
+    <h1 class="text-danger">Saint Selected</h1>
 
-{{-- table in cui stampare i dati del database con un foreach --}}
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Luogo Di Nascita</th>
-            <th scope="col">Data Della Benedizione</th>
-            <th scope="col">Numero Di Miracoli</th>
-        </tr>
-    </thead>
-    <tbody class="table-group-divider">
-                <tr>
-                    <th scope="row">{{ $saint ['name'] }}</th>
-                    <td>{{ $saint ['luogo_di_nascita'] }}</td>
-                    <td>{{ $saint ['data_denedizione'] }}</td>
-                    <td>{{ $saint ['numero_miracoli'] }}</td>
-                </tr>
-    </tbody>
-</table>
+    {{-- table in cui stampare i dati del database con un foreach --}}
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Luogo Di Nascita</th>
+                <th scope="col">Data Della Benedizione</th>
+                <th scope="col">Numero Di Miracoli</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+                    <tr>
+                        <th scope="row">{{ $saint ['name'] }}</th>
+                        <td>{{ $saint ['luogo_di_nascita'] }}</td>
+                        <td>{{ $saint ['data_denedizione'] }}</td>
+                        <td>{{ $saint ['numero_miracoli'] }}</td>
+                    </tr>
+        </tbody>
+    </table>
+
+    <div class="btn btn-danger ms_button">
+        <a href="/">Return to list</a>
+    </div>
 @endsection
